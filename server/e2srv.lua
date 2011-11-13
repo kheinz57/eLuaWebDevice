7ea6a3be8a01e7f4665c81a695b26c612bc5ab9f
unknown <Heinz@H1R500BITBAG.(none)>
2011-11-13 12:06:31 +0100

    put some notes in the readme file

--- server/e2srv.lua
+++ server/e2srv.lua
@@ -7,21 +7,17 @@ if arg then
 	host = arg[1] or host
 	port = arg[2] or port
 end
-print("Binding to host '" ..host.. "' and port " ..port.. "...")
 s = assert(socket.bind(host, port))
 i, p   = s:getsockname()
 assert(i, p)
 while true do
-    print("Waiting for connection from e2 device on " .. i .. ":" .. p .. "...")
+    print("====================================================================")
+    print("Waiting for connection from a eLUA device on " .. i .. ":" .. p .. "...")
     local c = assert(s:accept())
-    print("receiving hw-id from device.. ")
     local data, e = c:receive(8)
-    print("DBG: got from device:", data,e)
     if (#data > 0) then
         print("got request from device "..data)
         local filename = "../device/code/"..data..".lua"
---        local file = assert(io.open("../device/code/e2device.lua","r"))
---        local file = assert(io.open("../device/code/e2dev_"..data..".lua","r"))
         local file = io.open(filename,"r")
         if (file == nil) then
             file = io.open(filename, "w")
@@ -33,17 +29,16 @@ while true do
         local filelen=string.format("%05d",#content)
         file:close()
         print ("filelen="..filelen)
+        print("---------------------------------------------------------------------")
         print (content)
+        print("---------------------------------------------------------------------")
         print("Connected. Send len="..filelen)
-         os.execute("sleep 1")
-       c:send(filelen)
-        print("send returned ") 
+        os.execute("sleep 1")
+        c:send(filelen)
         os.execute("sleep 1")
         l, e = c:receive(2)
-        print("recv returned ") 
         os.execute("sleep 1")
         c:send(content)
-        print("send returned ") 
         os.execute("sleep 1")
         l, e = c:receive(2)
         c:close()
